24ca1050a4d9f0adef082a2fbf1699e9c5d969df
camiqf1 <156013609+camiqf1@users.noreply.github.com>
2025-01-18 01:24:46 +0000

    port

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,116 +1,113 @@
-import Image from 'next/image';
+import Navbar from "../components/Navbar/Navbar";
+import ArticleCard from "@/components/ArticleCard/ArticleCard";
+import Footer from "@/components/Footer/Footer";
+import ArticleWork from "@/components/ArticleWork/ArticleWork";
+import SignupWidget from "@/components/WigetSignup/SignupWidget";
+import ProfileHeader from "@/components/ProfileHeader/ProfileHeader";
+import profileIcon from "@/components/ProfileHeader/ProfileIcon/camila1.png";
+import SkillsWidget from "@/components/SkillsWidgets/SkillsWidgets";
+import htmlIcon from "../components/SkillsWidgets/icons/htm.png";
+import cssIcon from "../components/SkillsWidgets/icons/css.png";
+import javascriptIcon from "../components/SkillsWidgets/icons/javascript.png";
+import javaIcon from "../components/SkillsWidgets/icons/java.png"
+import Switcher from "../components/Switcher/Switcher";
 
+const articles = [
+  {
+    date: "Nov 15, 2021",
+    title: "Everything you need to know about React",
+    content:
+      "The ability to build stuff in React is one of the hottest skills to have today in software engineering. There is a lot of demand for React developers among startups as well as MNCs.",
+    link: "https://medium.com/the-research-nest/everything-you-need-to-know-about-react-ab24da4275ea",
+  },
+  {
+    date: "Jan 4, 2024",
+    title: "How to learn React step-by-step",
+    content:
+      "A software developer should learn React because it's widely used for building dynamic user interfaces, offers efficient development with its component-based approach, and enhances job opportunities due to its popularity in the industry.",
+    link: "https://www.freecodecamp.org/news/how-to-learn-react-step-by-step/",
+  },
+  {
+    date: "Sep 2, 2020",
+    title: "The Missing Introduction to React",
+    content:
+      "React is the world’s most popular JavaScript framework, but it’s not cool because it’s popular. It’s popular because it’s cool. Most React introductions jump right into showing you examples of how to use React, and skip the “why”.",
+    link: "https://medium.com/javascript-scene/the-missing-introduction-to-react-62837cb2fd76",
+  },
+];
+const works = [
+  { name: "Slack", icon: slack, startYear: 2016, endYear: "current" },
+  { name: "Spotify", icon: spotify, startYear: 2014, endYear: 2015 },
+  { name: "Microsoft", icon: micro, startYear: 2010, endYear: 2013 },
+];
+const skills = [
+  { name: "HTML", proficiency: 55, icon: htmlIcon },
+  { name: "CSS", proficiency: 45, icon: cssIcon },
+  { name: "JavaScript", proficiency: 65, icon: javascriptIcon },
+  { name: "java", proficiency: 75, icon: JavaIcon}
+];
+// >>>>>>> 54bdfa288e55094ea02f17b3eb14243f340e64e5
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p
-          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div
-          className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div
-        className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <Navbar
+        options={[
+          { path: "/", label: "Home" },
+          { path: "/about", label: "About" },
+          { path: "/projects", label: "Projects" },
+          { path: "/uses", label: "Uses" },
+        ]}
+      />
+      {/* <Switcher darkClassName="dark-mode" /> */}
+      <div className="profile-header-container">
+        <ProfileHeader
+          name="Software engineer, father, and believer"
+          bio="Dedicated software engineer with expertise in HTML, CSS, and nearly advanced JavaScript. With a keen eye for design and a knack for problem-solving, they craft elegant and functional web applications. Their proficiency in CSS and HTML ensures visually appealing and responsive user interfaces, while their growing mastery of JavaScript enables them to create dynamic and interactive web experiences. Continuously striving for excellence, [Name] is committed to honing their skills and staying abreast of the latest trends and technologies in the ever-evolving field of software development."
+          linkedinUrl="https://www.linkedin.com/in/yourprofile"
+          imageUrl={profileIcon} // Pass the imported profile icon image as a prop
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="footerContainer">
+        <div className="mainCard">
+          <div className="articles">
+            {articles.map((article, index) => (
+              <ArticleCard
+                key={index}
+                date={article.date}
+                title={article.title}
+                content={article.content}
+                link={article.link}
+              />
+            ))}
+          </div>
+          <div className="rightSidebar">
+            <SignupWidget
+              title={"Stay up to date"}
+              content={"Get notified when I publish something new, and unsubscribe at any time."}
+            />
+            <ArticleWork
+              title={"Work"}
+              content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi."}
+              items={works}
+            />
+            <SkillsWidget
+              title={"Skills"}
+              content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi."}
+              skills={skills}
+            />
+          </div>
+        </div>
+        <div className="footerBar">
+          <Footer
+            options={[
+              { path: "/", label: "Home" },
+              { path: "/about", label: "About" },
+              { path: "/projects", label: "Projects" },
+              { path: "/uses", label: "Uses" },
+            ]}
+          />
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,7 +1,46 @@
-export default function Home() {
+import ProjectHeader from "@/components/projectHeader/ProjectHeader";
+import ProjectsCard from "@/components/ProjectComponent/ProjectsCard";
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
+export const metadata = {
+  title: "Portfolio - Projects",
+  description: "The projects page of my portfolio.",
+};
+const cards = [1, 2, 3, 4, 5, 6];
+export default function Projects() {
   return (
-    <main>
-      Projects
-    </main>
+    <div>
+      <Navbar
+        options={[
+          { path: "/", label: "Home" },
+          { path: "/about", label: "About" },
+          { path: "/projects", label: "Projects" },
+          { path: "/uses", label: "Uses" },
+        ]}
+      />
+        <ProjectHeader
+        title="Things I’ve made trying to put my dent in the universe."
+        content="Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi.."
+      />
+    <div className="projects-layout">
+      <div className="cardAll">
+        {cards.map((card) => (
+          <div key={card}>
+            <ProjectsCard />
+          </div>
+        ))}
+      </div>
+    </div>
+    <div className="footerBar">
+          <Footer
+            options={[
+              { path: "/", label: "Home" },
+              { path: "/about", label: "About" },
+              { path: "/projects", label: "Projects" },
+              { path: "/uses", label: "Uses" },
+            ]}
+          />
+      </div>
+  </div>
   );
 }
